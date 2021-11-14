79b077961b14d7bf39325b0e89ce80d0a96b36e2
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-11-14 21:29:38 +0530

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Kushagra Sinha on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
